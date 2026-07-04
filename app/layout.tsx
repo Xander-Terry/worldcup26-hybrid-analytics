35e1e0d831fc77b42df2d74817ace84bbb03d861
Xander Terry <xandert@cs.washington.edu>
2026-07-04 14:02:14 -0700

    fixed icon syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,9 +48,9 @@ export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
   icons: {
-    icon: "/public/favicon.png",
-    shortcut: "/public/favicon.png",
-    apple: "/public/favicon.png",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
   },
 }
 
